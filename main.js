36e05c5aeb2ad57ed2e1d5be29dd71eb27295b5d
SampurnaGope <85182200+SampurnaGope@users.noreply.github.com>
2022-05-24 18:45:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function open_bedroom() {
+   
+    window.location = "bedroom.html";
+}
+function open_tv() {
+   
+    window.location = "tv.html";
+}
+function open_desk() {
+   
+    window.location = "desk.html";
+}
+function open_bottles() {
+   
+    window.location = "bottle.html";
+}
+function open_fruit() {
+   
+    window.location = "fruit.html";
+}
+
